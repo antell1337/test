61593318d2d1f67ec533e9513d33a09cf31034d5
antell1337 <oebobkov@edu.hse.ru>
2022-07-19 12:23:48 +0300

    initial version

---
+++ main.rb
@@ -0,0 +1,19 @@
+puts "Введите 1 число"
+first = gets.to_i
+puts "Введите 2 число"
+second = gets.to_i
+puts "Введите +, -, * или /"
+char = gets.chomp
+if char == '+'
+  puts "#{first + second}"
+elsif char == '-'
+  puts "#{first - second}"
+elsif char == '*'
+  puts "#{first * second}"
+elsif char == '/'
+  begin
+    puts "#{first / second}"
+  rescue ZeroDivisionError => error
+    puts "На ноль делить нельзя"
+  end
+end
